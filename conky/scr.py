0ac491beb9a6e05edd6062cc8f9a2c63cfa3edfe
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-07-20 00:52:06 +0300

    Create scr.py
    A file that collects info from https://www.instagram.com/bulletinoftheatomicscientists (the instagram page of https://thebulletin.org).
    Information that is needed is written in biography section, in capital letters only.

---
+++ conky/scr.py
@@ -0,0 +1,23 @@
+import urllib.request
+import json
+import re
+from bs4 import BeautifulSoup
+
+
+# Open a webpage and retrieve its content
+
+wp = urllib.request.urlopen('https://www.instagram.com/bulletinoftheatomicscientists/')
+wpText = wp.read().decode()
+
+# Find a needed part
+
+soup = BeautifulSoup(wpText, 'html.parser')
+text = soup.head.find('script', attrs={'type': 'application/ld+json'}).find_all(text=True, recursive=False)[0].strip()
+jsonObject = json.loads(text)
+
+ # Find all words in capital letters
+
+regex = r"\b[A-Z][A-Z]+\b"
+matches = re.finditer(regex, j['description'])
+
+print(' '.join([match[0] for match in matches]))
